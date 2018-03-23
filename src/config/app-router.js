b565789b84209d1d47da880f88e098ecc5d7ee3e
Carlos Contreras <carloscontreras0420@gmail.com>
2018-03-23 08:23:49 -0500

    Add pg package

--- src/config/app-router.js
+++ src/config/app-router.js
@@ -1,10 +1,11 @@
 import router from 'koa-router';
+import { greeting } from './db-connection';
 
 const appRouter = router();
 
 appRouter.get('/', async (ctx, next) => {
   // ctx.router available
-  ctx.body = 'Hello world 123';
+  ctx.body = await greeting();
 });
 
 export const routes = appRouter.routes();
